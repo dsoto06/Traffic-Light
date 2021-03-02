40be7aaa8f7cb198124750048a9606a3b5b9b3ce
dsoto06 <diegosr06@hotmail.com>
2021-03-02 04:49:29 +0000

    entrega

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 //create your first component
-export function TrafficLight() {
+export const TrafficLight = () => {
+	let [red, setRed] = useState("red");
+	let [yellow, setYellow] = useState("yellow");
+	let [green, setGreen] = useState("green");
+
+	let colorRed = () => {
+		if (red == "red") {
+			setRed("red color");
+		}
+		setYellow("yellow");
+		setGreen("green");
+	};
+
+	let colorYellow = () => {
+		if (yellow == "yellow") {
+			setYellow("yellow color");
+		}
+		setRed("red");
+		setGreen("green");
+	};
+
+	let colorGreen = () => {
+		if (green == "green") {
+			setGreen("green color");
+		}
+		setYellow("yellow");
+		setRed("red");
+	};
 	return (
 		<div>
 			<div className="line"></div>
 			<div className="box">
-				<div className="red"></div>
-				<div className="yellow"></div>
-				<div className="green"></div>
+				<div className="red" onClick={colorRed}>
+					{red}
+				</div>
+				<div className="yellow" onClick={colorYellow}>
+					{yellow}
+				</div>
+				<div className="green" onClick={colorGreen}>
+					{green}
+				</div>
 			</div>
 		</div>
 	);
-}
+};
